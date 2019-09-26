9aef5aeab2136a398c3df356aba3104a4c5d84e9
Fabrice Sznajderman <fabszn@protonmail.com>
2019-09-26 10:19:51 +0200

    clean code

--- week_2/07_ADT/src/main/scala/com/td/scala/adt/_3/Main.scala
+++ week_2/07_ADT/src/main/scala/com/td/scala/adt/_3/Main.scala
@@ -1,9 +1,5 @@
 package com.td.scala.adt._3
 
-import com.td.scala.adt._2.adt.types.kind.Chrome
-import com.td.scala.adt._2.adt.types.platform._
-import com.td.scala.adt._2.utils.ReleaseManager
-
 object Main extends App {
 
   // implement predicated here
