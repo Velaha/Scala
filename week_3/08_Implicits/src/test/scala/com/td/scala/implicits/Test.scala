95324cc71587a5691c75efdb7f7dc4aeb2ea03c6
Joris <>
2020-10-30 19:32:50 +0100

    Create dummy test for implicits

---
+++ week_3/08_Implicits/src/test/scala/com/td/scala/implicits/Test.scala
@@ -0,0 +1,18 @@
+package com.td.scala.implicits
+
+import collection.mutable.Stack
+import org.scalatest._
+import flatspec._
+import matchers._
+
+class Test extends AnyFlatSpec with should.Matchers {
+
+  // Test adapted from https://www.scalatest.org/ example
+  "A test" should "always pass" in {
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    stack.pop() should be(2)
+    stack.pop() should be(1)
+  }
+}
