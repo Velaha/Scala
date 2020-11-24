eb250f284c2ac1da0136b88635af7644dee2efa9
Joris <>
2020-11-24 15:34:37 +0100

    Add exercices with map, fold, ...

--- week_lists/fold/src/main/scala/com/td/Main.scala
+++ week_lists/fold/src/main/scala/com/td/Main.scala
@@ -1,6 +1,7 @@
 package com.td.fold
 
 object Main extends App {
+<<<<<<< ac412fe445ad3d9ce2c9066967e5b8eaea23be5a
   // See Data.trips and Data.names
 
   // Parse the String to LocalDateTime and then compute the duration of each trip
@@ -17,5 +18,29 @@ object Main extends App {
   // Hint: tripsAndName.foldLeft
   // result: (Kara, 6)
   val top1ByNumberOfTrips: (String, Int) = ???
+=======
+  // Playground
+
+   val tripsDuration: List[List[Long]] = Data.trips
+    .map(
+      _.map(Monitor.parse)
+        .map(Monitor.duration)
+    )
+
+    // result: 59280 minutes
+  val longestTripOfAll: Long = tripsDuration.flatten.max
+
+  val tripsAndName: List[(List[Long], String)] = tripsDuration
+    .zip(Data.names)
+
+    // result: (Kara, 6)
+  val top1ByNumberOfTrips: (String, Int) = tripsAndName.foldLeft(("", 0)) {
+    case (acc, tripsAndName) if tripsAndName._1.length > acc._2 => (tripsAndName._2, tripsAndName._1.length)
+    case (acc, tripsAndName) => acc
+  }
+
+  println(longestTripOfAll)
+  println( top1ByNumberOfTrips )
+>>>>>>> Add exercices with map, fold, ...
 
 }
