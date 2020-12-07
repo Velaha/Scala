4d0b670078a6c538e00bf3a2351fbc4587502daf
Joris <>
2020-12-07 09:43:56 +0100

    week_lists: Fix tests

--- week_lists/mylist/src/test/scala/com/td/mylist/Test.scala
+++ week_lists/mylist/src/test/scala/com/td/mylist/Test.scala
@@ -16,27 +16,28 @@ class Test extends AnyFlatSpec with Matchers {
   }
 
   it should "have a length of 0" in {
-    MyList.initEmpty().length == 0
+    MyList.initEmpty().length shouldBe (0)
+
   }
 
   it should "have no head element" in {
-    MyList.initEmpty().firstElement == None
+    MyList.initEmpty().firstElement shouldBe None
   }
 
   it should "have a no tail elements" in {
-    MyList.initEmpty().remainingElements == MyList.initEmpty()
+    MyList.initEmpty().remainingElements shouldBe MyList.initEmpty()
   }
 
   it should "empty string display correctly" in {
-    MyList.initEmpty().toPrint == ""
+    MyList.initEmpty().toPrint shouldBe ""
   }
 
   it should "map nothing" in {
-    MyList.initEmpty().map(_ => ()) == MyList.initEmpty()
+    MyList.initEmpty().map(_ => ()) shouldBe MyList.initEmpty()
   }
 
   it should "initiate a list" in {
-    MyList.initEmpty().addElement(3).length == 1
+    MyList.initEmpty().addElement(3).length shouldBe 1
   }
 
   "MyList" should "exists" in {
@@ -45,49 +46,49 @@ class Test extends AnyFlatSpec with Matchers {
 
   it should "initialize with 0 elements" in {
     MyList.init().isEmpty()
-    MyList.init() == End
-    MyList.init() == MyList.initEmpty()
+    MyList.init() shouldBe End
+    MyList.init() shouldBe MyList.initEmpty()
   }
 
   it should "initialize with 1 element" in {
-    MyList.init(3).length == 1
-    MyList.init(3) == Element(3, End)
+    MyList.init(3).length shouldBe 1
+    MyList.init(3) shouldBe Element(3, End)
   }
 
   it should "initialize with multiple elements" in {
-    MyList.init(3, 4).length == 2
-    MyList.init(3) == Element(3, Element(4, End))
+    MyList.init(3, 4).length shouldBe 2
+    MyList.init(3, 4) shouldBe Element(3, Element(4, End))
   }
 
   it should "have head element" in {
-    MyList.init(3).firstElement == Some(3)
-    MyList.init(3, 4).firstElement == Some(3)
+    MyList.init(3).firstElement shouldBe Some(3)
+    MyList.init(3, 4).firstElement shouldBe Some(3)
   }
 
   it should "have a tail elements" in {
-    MyList.init(3).remainingElements == End
-    MyList.init(3, 4).remainingElements == Element(4, End)
+    MyList.init(3).remainingElements shouldBe End
+    MyList.init(3, 4).remainingElements shouldBe Element(4, End)
   }
 
   it should "empty string display correctly" in {
-    MyList.init(3).toPrint == "3"
-    MyList.init(3, 4).toPrint == "3, 4"
+    MyList.init(3).toPrint shouldBe "3"
+    MyList.init(3, 4).toPrint shouldBe "3, 4"
   }
 
   it should "map nothing" in {
     val f: Int => Int = _ * 2
-    MyList.init(3).map(f) == MyList.init(f(3))
-    MyList.init(3, 4).map(f) == MyList.init(f(3), f(4))
+    MyList.init(3).map(f) shouldBe MyList.init(f(3))
+    MyList.init(3, 4).map(f) shouldBe MyList.init(f(3), f(4))
   }
 
   it should "add an element" in {
-    MyList.init(3).addElement(5) == MyList.init(3, 5)
-    MyList.init(3, 4).addElement(5) == MyList.init(3, 4, 5)
+    MyList.init(3).addElement(5) shouldBe (MyList.init(5, 3))
+    MyList.init(3, 4).addElement(5) shouldBe MyList.init(5, 3, 4)
   }
 
   it should "have apply" in {
-    MyList(3) == MyList.init(3)
-    MyList() == End
+    MyList(3) shouldBe MyList.init(3)
+    MyList() shouldBe End
   }
 
 }
