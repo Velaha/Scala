cd9dd412a0359102e47285d748157ff415f8c5de
Joris <>
2020-12-07 09:50:53 +0100

    week_lists: Fix error merge

--- week_lists/fold/src/main/scala/com/td/Monitor.scala
+++ week_lists/fold/src/main/scala/com/td/Monitor.scala
@@ -10,20 +10,9 @@ object Monitor {
   // def parse(list: List[List[(String, String)]]): List[List[(LocalDateTime, LocalDateTime)]] =
   // but
   // TODO: implement a function parse: ((String, String)) => (LocalDateTime, LocalDateTime)
-<<<<<<< ac412fe445ad3d9ce2c9066967e5b8eaea23be5a
   val parse: ((String, String)) => (LocalDateTime, LocalDateTime) = ???
 
   // TODO: implement a function parse: ((String, String)) => (LocalDateTime, LocalDateTime)
   val duration: ((LocalDateTime, LocalDateTime)) => Long = ???
-=======
-  val parse: ((String, String)) => (LocalDateTime, LocalDateTime) = { case (start, end) =>
-    (LocalDateTime.parse(start), LocalDateTime.parse(end))
-  }
-
-  // TODO: implement a function parse: ((String, String)) => (LocalDateTime, LocalDateTime)
-  val duration: ((LocalDateTime, LocalDateTime)) => Long = { case (start, end) =>
-    ChronoUnit.SECONDS.between(start, end)
-  }
->>>>>>> Add exercices with map, fold, ...
 
 }
