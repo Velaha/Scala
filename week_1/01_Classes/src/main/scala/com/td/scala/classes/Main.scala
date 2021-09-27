619874b8534db4ff4b978c91d0f4cb71c630fcec
Fabrice Sznajderman <fabszn@protonmail.com>
2021-09-27 17:18:13 +0200

    fix issues

--- week_1/01_Classes/src/main/scala/com/td/scala/classes/Main.scala
+++ week_1/01_Classes/src/main/scala/com/td/scala/classes/Main.scala
@@ -2,17 +2,5 @@ package com.td.scala.classes
 
 object Main extends App {
 
-  println("Drive your car here")
-
-  val car1 = new Car(3, "white", 4)
-
-  println(car1.color)
-
-  car1.speedUp(10)
-
-  println(car1.speed)
-
-  car1.brake(-8)
-
-  println(car1.speed)
+ 
 }
