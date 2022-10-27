23f6636f8e4436be2861e3f8efbe72699d327d3a
aheude <aheude>
2022-10-27 13:14:58 +0200

    Done first exercise of week_1 Modify .gitignore

---
+++ week_1/01_Classes/src/main/scala/com/td/scala/classes/Car.scala
@@ -0,0 +1,20 @@
+package com.td.scala.classes
+
+class Car (private val nbDoors: Int, var color: String, val nbPlaces: Int, var speed: Int = 0){
+    def this(nbDoors: Int, nbPlaces: Int) = this(nbDoors, "blue", nbPlaces)
+
+
+    def speedUp(increase: Int): Unit = {
+        if (increase < 0) {
+            throw new IllegalArgumentException("Increase must be positive")
+        }
+        speed += increase
+    }
+
+    def break(decrease: Int): Unit = {
+        if (decrease < 0) {
+            throw new IllegalArgumentException("Decrease must be positive")
+        }
+        speed -= decrease
+    }
+}
--- week_1/01_Classes/src/main/scala/com/td/scala/classes/Main.scala
+++ week_1/01_Classes/src/main/scala/com/td/scala/classes/Main.scala
@@ -1,6 +1,15 @@
 package com.td.scala.classes
 
 object Main extends App {
+    val blueCar = new Car(2, "blue", 4)
+    val blackCar = new Car(4, 5)
 
- 
+    blueCar.speedUp(15)
+    blueCar.break(10)
+    println(blueCar.speed)
+
+    //blackCar.break(-1)
+    //blackCar.speedUp(-10)
+
+
 }
